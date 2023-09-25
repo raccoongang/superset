6495d343c25aae2ffce845e9eec7da1151f6de80
nikolay.borovenskiy <nikolay.borovenskiy@gmail.com>
2023-09-25 20:25:48 +0300

    feat: [ZNO-2072] Implements PDF and Word export command

---
+++ superset/dashboards/data/commands/exceptions.py
@@ -0,0 +1,9 @@
+from flask_babel import lazy_gettext as _
+
+from superset.commands.exceptions import (
+    CommandException,
+)
+
+
+class PDFGenerationFailedError(CommandException):
+    message = _("Generation PDF failed for an unknown reason")

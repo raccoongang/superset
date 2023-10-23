a8bf7c97007744210758922ffb6692d6ccb1a7d5
NikolayBorovenskiy <nikolay.borovenskiy@gmail.com>
2023-10-23 17:23:13 +0300

    chore: [ZNO-2101] Extens content security policy for image sources (#21)

--- superset/config.py
+++ superset/config.py
@@ -1414,7 +1414,7 @@ TALISMAN_ENABLED = utils.cast_to_boolean(os.environ.get("TALISMAN_ENABLED", True
 TALISMAN_CONFIG = {
     "content_security_policy": {
         "default-src": ["'self'"],
-        "img-src": ["'self'", "data:"],
+        "img-src": ["'self'", "https:", "data:"],
         "worker-src": ["'self'", "blob:"],
         "connect-src": [
             "'self'",
@@ -1432,7 +1432,7 @@ TALISMAN_CONFIG = {
 TALISMAN_DEV_CONFIG = {
     "content_security_policy": {
         "default-src": ["'self'"],
-        "img-src": ["'self'", "data:"],
+        "img-src": ["'self'", "https:", "data:"],
         "worker-src": ["'self'", "blob:"],
         "connect-src": [
             "'self'",

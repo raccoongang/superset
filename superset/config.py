79ab2a5e2b9272c4921cf1b5bf34f0bc5e14ae79
Oleksandr Marenych <sendr84@gmail.com>
2025-10-07 12:40:14 +0300

    fix: correct and enable uk translations

--- superset/config.py
+++ superset/config.py
@@ -370,7 +370,7 @@ PUBLIC_ROLE_LIKE: str | None = None
 # Babel config for translations
 # ---------------------------------------------------
 # Setup default language
-BABEL_DEFAULT_LOCALE = "en"
+BABEL_DEFAULT_LOCALE = "uk"
 # Your application default translation path
 BABEL_DEFAULT_FOLDER = "superset/translations"
 # The allowed translation for your app
@@ -394,7 +394,10 @@ LANGUAGES = {
 }
 # Turning off i18n by default as translation in most languages are
 # incomplete and not well maintained.
-LANGUAGES = {}
+LANGUAGES = {
+    "uk": {"flag": "uk", "name": "Ukranian"},
+    "en": {"flag": "us", "name": "English"},
+}
 
 
 # Override the default d3 locale format
